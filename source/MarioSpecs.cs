a24c414dc76b6bec3e1e0e63836e37e17cd3902e
Uli Armbruster <ua@co-IT.eu>
2017-10-12 04:23:34 +0200

    API First mit TDD: Iteration 1

---
+++ source/MarioSpecs.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace SuperMarioKata
+{
+    public class MarioSpecs
+    {
+        [Fact]
+        public void Der_kleine_Mario_stirbt_wenn_er_vom_Gegner_getroffen_wird()
+        {
+            var mario = new Mario();
+            var getroffenerMario = mario.WirdVonGegnerGetroffen();
+            getroffenerMario.Should().Be(Mario.ToterMario());
+
+        }
+    }
+
+    public class Mario
+    {
+        public static Mario ToterMario()
+        {
+            return new Mario();
+        }
+
+        public Mario WirdVonGegnerGetroffen()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
