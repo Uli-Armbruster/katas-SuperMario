62d3cf186638654c63ef01f79db7ad2335caa873
Uli Armbruster <ua@co-IT.eu>
2018-05-15 22:42:17 +0200

    Iteration 1: Konzepte 'Treffer durch Gegner' und 'Sterben' umgesetzt

--- source/SuperMario.cs
+++ source/SuperMario.cs
@@ -1,4 +1,33 @@
 ﻿namespace SuperMarioRefactoring
 {
-  class SuperMario { }
+  public class SuperMario
+  {
+    public SuperMario()
+    {
+      IstTot = false;
+      IstGroß = false;
+    }
+
+    public bool IstTot { get; private set; }
+    private bool IstGroß { get; set; }
+
+    public void WirdVonGegnerGetroffen()
+    {
+      if (IstGroß)
+      {
+        IstGroß = false;
+        return;
+      }
+
+      IstTot = true;
+    }
+
+    public void FindetPilz()
+    {
+      if (IstTot)
+        return;
+
+      IstGroß = true;
+    }
+  }
 }
