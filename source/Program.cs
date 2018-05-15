cf7ce3df4f6646f370e591f1f3d9ad93e8981138
Uli Armbruster <ua@co-IT.eu>
2018-05-15 22:26:01 +0200

    Leere Solution hinzugefügt

---
+++ source/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarioRefactoring
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
---
+++ source/SuperMario.cs
@@ -0,0 +1,4 @@
+﻿namespace SuperMarioRefactoring
+{
+  class SuperMario { }
+}
