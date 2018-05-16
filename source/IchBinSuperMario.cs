09a07f7fe6c85b7607ca6f008ad44b43460da05c
Uli Armbruster <ua@co-IT.eu>
2018-05-16 15:51:39 +0200

    Funktionen in Objekte überführen (2) | Benefit: Abstraktion entkoppelt (DI Principle)

---
+++ source/IchBinSuperMario.cs
@@ -0,0 +1,16 @@
+﻿namespace SuperMarioRefactoring
+{
+  public interface IchBinSuperMario
+  {
+    int AnzahlLeben { get; }
+    bool BesitztYoshi { get; }
+    Status Status { get; }
+
+    SuperMario WirdVonGegnerGetroffen();
+    SuperMario FindetLeben();
+    SuperMario FindetPilz();
+    SuperMario FindetFeuerblume();
+    SuperMario FindetYoshi();
+    SuperMario FälltInLoch();
+  }
+}
