69961b5a814fcd5f011b8d137e45a0fd56b58c6b
Uli Armbruster <ua@co-IT.eu>
2018-05-16 17:24:09 +0200

    Benefit: State Pattern ermöglich neuen Status 'Mario mit Yoshi'

---
+++ source/MarioMitYoshi.cs
@@ -0,0 +1,49 @@
+﻿namespace SuperMarioRefactoring
+{
+  class MarioMitYoshi : IchBinSuperMario
+  {
+    private readonly IchBinSuperMario _reiter;
+
+    public MarioMitYoshi(IchBinSuperMario reiter, int anzahlLeben)
+    {
+      AnzahlLeben = anzahlLeben;
+      _reiter = reiter;
+    }
+
+    public int AnzahlLeben { get; }
+    public bool BesitztYoshi { get; }
+
+    public IchBinSuperMario WirdVonGegnerGetroffen()
+    {
+      return _reiter;
+    }
+
+    public IchBinSuperMario FindetLeben()
+    {
+      _reiter.FindetLeben();
+      return this;
+    }
+
+    public IchBinSuperMario FindetPilz()
+    {
+      _reiter.FindetPilz();
+      return this;
+    }
+
+    public IchBinSuperMario FindetFeuerblume()
+    {
+      _reiter.FindetFeuerblume();
+      return this;
+    }
+
+    public IchBinSuperMario FindetYoshi()
+    {
+      return this;
+    }
+
+    public IchBinSuperMario FälltInLoch()
+    {
+      return _reiter.FälltInLoch();
+    }
+  }
+}
