2cd445e09a3f23a53e10869de5417dedd2b86518
Uli Armbruster <ua@co-IT.eu>
2018-05-16 16:29:01 +0200

    Funktionen in Objekte überführen (4) | Benefit: Domänensprache spiegelt sich in Code (State Pattern)

---
+++ source/KleinerMario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SuperMarioRefactoring
+{
+  class KleinerMario : IchBinSuperMario
+  {
+    public KleinerMario(int anzahlLeben)
+    {
+      AnzahlLeben = anzahlLeben;
+    }
+
+    public int AnzahlLeben { get; }
+    public bool BesitztYoshi { get; }
+    public Status Status { get; }
+    public IchBinSuperMario WirdVonGegnerGetroffen()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FindetLeben()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FindetPilz()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FindetFeuerblume()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FindetYoshi()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FälltInLoch()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ source/MarioMitPilz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SuperMarioRefactoring
+{
+  class MarioMitPilz : IchBinSuperMario
+  {
+    public int AnzahlLeben { get; }
+    public bool BesitztYoshi { get; }
+    public Status Status { get; }
+
+    public MarioMitPilz(int anzahlLeben)
+    {
+      AnzahlLeben = anzahlLeben;
+    }
+
+    public IchBinSuperMario WirdVonGegnerGetroffen()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FindetLeben()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FindetPilz()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FindetFeuerblume()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FindetYoshi()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IchBinSuperMario FälltInLoch()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
